c6e2363d3c6ee1433aa5607d259afab627718da8
Felix Harrison <fmlharrison@gmail.com>
2017-02-03 12:14:53 +0000

    Added the views

--- BookServiceAPI/Scripts/app.js
+++ BookServiceAPI/Scripts/app.js
@@ -1,17 +1,27 @@
 ﻿var ViewModel = function() {
     var self = this;
-    self.books = ko.observableArray;
-    self.error = ko.observable;
+    self.books = ko.observableArray();
+    self.error = ko.observable();
+    self.detail = ko.observable();
+    self.authors = ko.observableArray();
+    self.newBook = {
+        Author: ko.observable(),
+        Genre: ko.observable(),
+        Price: ko.observable(),
+        Title: ko.observable(),
+        Year: ko.observable()
+    }
 
     var booksUri = "/api/books/";
+    var authorsUri = "/api/authors";
 
     function ajaxHelper(uri, method, data) {
-        self.error(''); //This clears the error message
+        self.error("");//This clears the error message
         return $.ajax({
             type: method,
             url: uri,
-            dataType: 'json',
-            contentType: 'application/json',
+            dataType: "json",
+            contentType: "application/json",
             data: data ? JSON.stringify(data) : null
         }).fail(function(jqXHR, textStatus, errorThrown) {
             self.error(errorThrown);
@@ -19,13 +29,40 @@
     }
 
     function getAllBooks() {
-        ajaxHelper(booksUri, 'GET').done(function(data) {
+        ajaxHelper(booksUri, "GET").done(function (data) {
             self.books(data);
+        });
+    }
+
+    function getAuthors() {
+        ajaxHelper(authorsUri, "GET").done(function(data) {
+            self.authors(data);
+        });
+    }
+
+    self.getBookDetail = function (item) {
+        ajaxHelper(booksUri + item.Id, "GET").done(function(data) {
+            self.detail(data);
+        });
+    }
+
+    self.addBook = function(formElement) {
+        var book = {
+            AuthorId: self.newBook.Author().Id,
+            Genre: self.newBook.Genre(),
+            Price: self.newBook.Price(),
+            Title: self.newBook.Title(),
+            Year: self.newBook.Year()
+        };
+
+        ajaxHelper(booksUri, "POST", book).done(function(item) {
+            self.books.push(item);
         });
     }
 
     //Fetch the initial data
     getAllBooks();
+    getAuthors();
 };
 
 //It takes the view model as a parameter and sets up the data binding.
